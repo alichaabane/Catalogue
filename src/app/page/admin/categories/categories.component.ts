10a3bc719a037970e18eb9c5b4537998d7e78eaa
Ali Chaabane <milik9898>
2022-05-15 15:42:59 +0100

    fix

--- src/app/page/admin/categories/categories.component.ts
+++ src/app/page/admin/categories/categories.component.ts
@@ -10,24 +10,36 @@ import {Router} from "@angular/router";
 export class CategoriesComponent implements OnInit {
 
   categories: any[] = [];
+  clickedIndex = 0;
 
   constructor(private catalogueService: CatalogueService, private router: Router) { }
 
   ngOnInit(): void {
     this.catalogueService.getCategories().subscribe((res: any) => {
     this.categories =  res._embedded.categories;
-    console.log(this.categories);
+      console.log(this.categories);
+      this.init();
     }, error => {
       console.log("erreur lors de récuperation des categories : ", error);
     }, () => {
-      console.log("complete ...");
+      // console.log("complete ...");
     })
   }
 
+  init() {
+    this.onGetProducts(this.categories[this.clickedIndex]);
+   // show first category products by defauly
+  }
   onGetProducts(c: any): void {
     console.log(c._links.products.href);
   let url = c._links.products.href;
-  this.router.navigateByUrl('/products/' + btoa(url));
+  this.router.navigateByUrl('/products/' + btoa(url)).then();
   }
+
+  changeState(index: any) {
+    console.log(index);
+    this.clickedIndex = index;
+  }
+
 
 }
